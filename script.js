e3d08c7b857ab1e3208798f51312a84a987f5860
Kemalelee <pikapika3072@gmail.com>
2021-08-01 17:35:21 +0600

    init commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let query = '{\
+    progress(where: {user:{login:{_eq:"Kemalelee"}}}){\
+        object{\
+        name\
+      }\
+      createdAt\
+      grade\
+    }\
+  }'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Server is live!")
