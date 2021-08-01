995ea164e2df448aefe3e369758013c91fd5921c
Kemalelee <pikapika3072@gmail.com>
2021-08-01 18:08:10 +0600

    add: fetch progress

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-let query = '{\
-    progress(where: {user:{login:{_eq:"Kemalelee"}}}){\
-        object{\
-        name\
-      }\
-      createdAt\
-      grade\
-    }\
-  }'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 console.log("Server is live!")
+let api = 'https://01.alem.school/api/graphql-engine/v1/graphql'
+
+const getProgress = async(url) => {
+  let query = `{
+    progress(where: {user:{login:{_eq:"Kemalelee"}}}, offset:48){
+        object{
+        name
+      }
+      createdAt
+      grade
+    }
+  }`
+
+ let result = await fetch(`${api}`,{
+  method: 'POST',
+  headers: {'Content-Type' : 'application/json'},
+  body: JSON.stringify({query: query})
+  })
+  .then(resp => resp.json())
+  .catch(err=>{console.log('err:', err)})
+
+  return result.data
+}
+
+const init = async () => {
+  console.log(await getProgress(api))
+}
+
+init()
